d74707ee03ae9b570cce1a7d28bba81212f55037
Duda <maria.avila.januario@escola.pr.gov.br>
2026-05-01 11:06:08 -0300

    Add files via upload

---
+++ app/outrosCubos.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+
+export default function outrosCubos(){
+    return (
+        <View style={{height: '100%'}}>
+            <View
+                style={{height: '15%', backgroundColor: '#1b684a'}}/>
+            <View
+                style={{width: '66%', height: '35%', backgroundColor: '#489276'}}/>
+            <View
+                style={{width: '33%', height: '50%', backgroundColor: '#6cac93'}}/>
+        </View>
+    );
+    
+};
